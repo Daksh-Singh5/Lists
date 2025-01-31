4801b91455bf03e0c6af6297a5628dfb47fef6d1
Daksh-Singh5 <dakshsingh6612@gmail.com>
2025-01-31 19:48:15 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,7 @@
+List = ["Wow",2,7,324,1,"yellow"]
+print(List)
+List.append("yay")
+print(List)
+List.pop()
+print(List)
+print(len(List))
---
+++ index2.py
@@ -0,0 +1,10 @@
+list=[1,2,3,4,5,6,7,8,9,10,11]
+sum = 0
+average = 0
+for i in list:
+    sum+=i
+
+
+print (sum)
+average = sum/len(list)
+print(average)
